c50d1afb7e4652cb78346efce8d76bf996596ddf
lukesherwood <luke.sherwood55@gmail.com>
2020-05-01 15:32:03 +1200

    added index pages and basic functionality of site

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :events
   resources :organisations
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "application#index"
+  root to: "events#index"
 end
