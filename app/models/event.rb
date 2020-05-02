c8b30f2a96bdd5aca96901912a3c8581b26f83ab
lukesherwood <luke.sherwood55@gmail.com>
2020-05-02 15:16:16 +1200

    fixed datetime display in views

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,5 +4,9 @@ class Event < ApplicationRecord
   has_many :volunteers, through: :jobs, foreign_key: "user_id"
   
   validates :name, :date, :location,  presence: true
+
+  def datetime
+    self.date.strftime("%l:%M %p %A %m %b %Y")
+  end
 end
 
