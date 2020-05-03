a44561185e8732bdcf8365c77cebc09c7a369ada
lukesherwood <luke.sherwood55@gmail.com>
2020-05-04 09:57:14 +1200

    added show page for org and added links to index

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -3,4 +3,8 @@ class OrganisationsController < ApplicationController
     def index
         @orgs = Organisation.all
     end
+
+    def show
+        @organisation = Organisation.find(params[:id])
+    end
 end
