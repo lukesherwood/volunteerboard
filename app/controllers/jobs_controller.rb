c6cde2266990bde6889cf03859656cfa654bf349
lukesherwood <luke.sherwood55@gmail.com>
2020-05-06 10:45:50 +1200

    implementing delete links on events and jobs views

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,6 @@
 class JobsController < ApplicationController
-    before_action :set_job, only: [:show, :edit, :update, :delete]
-    before_action :set_event, only: [:create, new, :edit]
+    before_action :set_job, only: [:show, :edit, :update, :destroy]
+    before_action :set_event, only: [:index, :create, :new, :edit, :update]
 
     def show
         @organisation = @job.organisation
@@ -13,19 +13,19 @@ class JobsController < ApplicationController
     def index
         if params[:organisation_slug]
             @organisation = Organisation.find_by_slug(params[:organisation_slug])
-            @jobs = @organisation.jobs.available
+            @jobs = @organisation.jobs
         elsif params[:event_id]
             @event = Event.find(params[:event_id])
-            @jobs = @event.jobs.available
+            @jobs = @event.jobs
         else
-            @jobs = Job.all.available
+            @jobs = Job.all
         end
     end
 
     def create
         @job = @event.jobs.build(job_params)
         if @job.save
-            redirect_to event_job_path(@job)
+            redirect_to event_job_path(@event, @job)
         else
             render 'new'
         end
@@ -36,10 +36,15 @@ class JobsController < ApplicationController
 
     def update
         if @job.update(job_params)
-            redirect_to event_job_path(@job)
+            redirect_to event_job_path(@event, @job)
         else
             render 'edit'
         end
+    end
+
+    def destroy
+        @job.destroy
+        redirect_to event_jobs_path
     end
 
     private
