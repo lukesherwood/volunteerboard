4c382812ff93f5e661977ec44e348d0235862fd6
lukesherwood <luke.sherwood55@gmail.com>
2020-05-11 12:02:51 +1200

    added validations to jobs - need volunteer if assigned and viceversa

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,6 +3,8 @@ class Job < ApplicationRecord
     belongs_to :volunteer, optional:true, class_name: "User", foreign_key: "user_id"
     validates :title, presence: true
     validates :description, presence: true
+    validates :volunteer, presence: { message: "must be provided if job is assigned" }, if: -> { assigned } 
+    validates :volunteer, absence: { message: "must be blank if job is un-assigned" }, if: -> { !assigned } 
     scope :available, -> { where(assigned: false) }
 
     def organisation
